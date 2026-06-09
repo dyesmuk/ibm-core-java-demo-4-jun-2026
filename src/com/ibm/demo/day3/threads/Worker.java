00aadf5da033a037ac9eb326b6c87a8ba3af2f87
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-06-09 09:46:39 +0530

    new features

--- src/com/ibm/demo/day3/threads/Worker.java
+++ src/com/ibm/demo/day3/threads/Worker.java
@@ -1,9 +1,14 @@
 package com.ibm.demo.day3.threads;
 
 class Worker implements Runnable {
+//class Worker extends Thread {
 
 	@Override
 	public void run() {
+		method();
+	}
+
+	public void method() {
 
 		for (int i = 1; i <= 3; i++) {
 
